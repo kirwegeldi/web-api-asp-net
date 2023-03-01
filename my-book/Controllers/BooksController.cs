100fd085cc60427b022f71e06f03c547b50569c6
Serif Aksoy <aksoycoding@gmail.com>
2023-03-01 18:54:30 +0300

    HTTP POST -> Adding new book to database

---
+++ my-book/Controllers/BooksController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using my_book.Data.Services;
+using my_book.Data.ViewModels;
+
+namespace my_book.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        public BooksService _booksService;
+        public BooksController(BooksService booksService)
+        {
+            _booksService = booksService;
+        }
+        [HttpPost("add-book")]
+        public IActionResult AddBook([FromBody]BookVM book) 
+        {
+            _booksService.AddBook(book);
+            return Ok();
+        }
+    }
+}
---
+++ my-book/Data/Services/BooksService.cs
@@ -0,0 +1,34 @@
+﻿using my_book.Data.Models;
+using my_book.Data.ViewModels;
+using System;
+
+namespace my_book.Data.Services
+{
+    public class BooksService
+    {
+        private AppDbContext _context;
+        public BooksService(AppDbContext context)
+        {
+
+            _context = context;
+
+        }
+        public void AddBook(BookVM book)
+        {
+            var _book = new Book()
+            {
+                Title = book.Title,
+                Description = book.Description,
+                IsRead = book.IsRead,
+                DateRead = book.IsRead ? book.DateRead.Value : null,
+                Rate = book.IsRead ? book.Rate.Value : null,
+                Genre = book.Genre,
+                Author = book.Author,
+                CoverUrl = book.CoverUrl,
+                DateAdded = DateTime.Now
+            };
+            _context.Books.Add(_book);
+            _context.SaveChanges();
+        }
+    }
+}
